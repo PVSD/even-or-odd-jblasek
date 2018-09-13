3ee9bdd7096a6448c572867d6a300c3c731da387
jblasek <jacob.blasek1220@gmail.com>
2018-09-13 16:49:52 -0400

    i found a new friend today. Sadly I found him in a ditch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner kbReaderInt = new Scanner(System.in);
+
+        System.out.println("Enter a Number.");
+        int intRead = kbReaderInt.nextInt();
+        if (intRead % 2 == 0){
+            System.out.println("The integer " + intRead + " is even.");
+        }
+        else {
+            System.out.println("The integer " + intRead + " is odd.");
+        }
     }
 }
